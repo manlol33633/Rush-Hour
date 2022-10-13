3bb85b4bc1f1ab8afc497c889d4bac62753f7ed6
manlol33633 <51886476+manlol33633@users.noreply.github.com>
2022-10-12 23:46:34 -0500

    Chnages

--- Assets/Scripts/SingleAxisMovement.cs
+++ Assets/Scripts/SingleAxisMovement.cs
@@ -8,9 +8,11 @@ public class SingleAxisMovement : MonoBehaviour
     [SerializeField] private float xVelocity;
     [SerializeField] private float yVelocity;
     [SerializeField] private Vector2 speed;
+    [SerializeField] private int[] rangeMap = new int[2];
     private Vector3 mousePosition;
     private string Axis;
-    private RaycastHit2D hitOrMiss;
+    private RaycastHit2D mouseCast;
+    private RaycastHit2D[] vehicleCast = new RaycastHit2D[2];
     private Vector2 mousePositionY;
     private bool mouseDown;
     private Vector2 endPoint; 
@@ -19,31 +21,38 @@ public class SingleAxisMovement : MonoBehaviour
     }
 
     void Update() {
-        if (hitOrMiss.collider != null) {
-            if (hitOrMiss.collider.gameObject.tag == "Car" && Input.GetMouseButtonDown(0)) {
-                mouseDown = true;
-            } else if (Input.GetMouseButtonUp(0)) {
-                mouseDown = false;
-            }
-        }
-        
         Vector3 mousePosition3D = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2 mousePosition = new Vector2(mousePosition3D.x, mousePosition3D.y);
+        
 
-        hitOrMiss = Physics2D.Raycast(mousePosition, Vector2.zero);
-
+        mouseCast = Physics2D.Raycast(mousePosition, Vector2.zero);
+        vehicleCast[0] = Physics2D.Raycast(transform.position, Vector2.down);
+        vehicleCast[1] = Physics2D.Raycast(transform.position, Vector2.up);
+    
         mousePositionY = new Vector2(transform.position.x, mousePosition.y);
 
         endPoint = new Vector2(transform.position.x, mousePosition.y);
+
+        
     }
 
     void FixedUpdate() {
-        if (mouseDown) {
-            
-        }
+        
     }
 
     void OnMouseDrag() {
-        rb2d.MovePosition(rb2d.position + speed * Time.fixedDeltaTime);
+        if (mousePositionY.y >= rangeMap[0] && mousePositionY.y <= rangeMap[1]) {
+            rb2d.MovePosition(mousePositionY);
+        }
+        if (mousePositionY.y <= rangeMap[0]) {
+            transform.position = new Vector2(transform.position.x, rangeMap[0]);
+        } else if (mousePositionY.y >= rangeMap[1]) {
+            transform.position = new Vector2(transform.position.x, rangeMap[1]);
+        }
+        if (vehicleCast[0].collider != null) {
+            if (vehicleCast[0].collider.gameObject.tag == "Barrier") {
+                Debug.Log(vehicleCast[0].distance);
+            }
+        }
     }
 }
