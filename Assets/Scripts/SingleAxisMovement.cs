52f461b1bbe147e2a751e5dd5c21e13e0ca908d2
manlol33633 <51886476+manlol33633@users.noreply.github.com>
2022-10-14 08:05:02 -0500

    fbiosrpg

--- Assets/Scripts/SingleAxisMovement.cs
+++ Assets/Scripts/SingleAxisMovement.cs
@@ -9,6 +9,7 @@ public class SingleAxisMovement : MonoBehaviour
     [SerializeField] private float yVelocity;
     [SerializeField] private Vector2 speed;
     [SerializeField] private int[] rangeMap = new int[2];
+    [SerializeField] private int carType;
     private Vector3 mousePosition;
     private string Axis;
     private RaycastHit2D mouseCast;
@@ -16,7 +17,7 @@ public class SingleAxisMovement : MonoBehaviour
     private Vector2[] vehicleCastPoint = new Vector2[2];
     private Vector2 mousePositionY;
     private bool mouseDown;
-    private Vector2 endPoint; 
+    private Vector2 endPoint;
     void Start() {
         rb2d = GetComponent<Rigidbody2D>();
     }
@@ -24,10 +25,9 @@ public class SingleAxisMovement : MonoBehaviour
     void Update() {
         Vector3 mousePosition3D = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2 mousePosition = new Vector2(mousePosition3D.x, mousePosition3D.y);
-        vehicleCastPoint[0] = new Vector2(transform.position.x, transform.position.y - 1);
-        vehicleCastPoint[1] = new Vector2(transform.position.x, transform.position.y + 1);
+        vehicleCastPoint[0] = new Vector2(transform.position.x, transform.position.y - 2);
+        vehicleCastPoint[1] = new Vector2(transform.position.x, transform.position.y + 2);
         
-
         mouseCast = Physics2D.Raycast(mousePosition, Vector2.zero);
         vehicleCast[0] = Physics2D.Raycast(vehicleCastPoint[0], Vector2.down);
         vehicleCast[1] = Physics2D.Raycast(vehicleCastPoint[1], Vector2.up);
@@ -53,9 +53,15 @@ public class SingleAxisMovement : MonoBehaviour
             transform.position = new Vector2(transform.position.x, rangeMap[1]);
         }
         if (vehicleCast[0].collider != null) {
-            if (vehicleCast[0].collider.gameObject.tag == "Barrier") {
-                Debug.Log(vehicleCast[0].distance);
+            if (vehicleCast[0].collider.gameObject.tag == "Car") {
+                if (vehicleCast[0].distance <= 0.1) {
+
+                }
             }
         } 
+    }
+
+    void lockOn() {
+        
     }
 }
